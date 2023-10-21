0e9d50b1e3c15fa61679c5963f6a883b6d43464a
fabio2100 <squizzato.fabio@gmail.com>
2023-10-20 21:02:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [paises,setPaises] = useState();
+  const [randomPaises,setRandomPaises] = useState();
+  const [elegidos,setElegidos] = useState();
+  const [bandera,setBandera] = useState();
+
+  useEffect(()=>{
+    fetch('https://flagcdn.com/es/codes.json')
+    .then(response => response.json())
+    .then(data => {console.log({data});setPaises(data)})
+  },[])
+
+  const handleClick = () => {
+    let paisesSeleccionados = [];
+    for(var i=0;i<3;i++){
+      const randomPais = Object.keys(paises)[Math.floor(Math.random()*Object.keys(paises).length)];
+      paisesSeleccionados.push({codigo:randomPais,nombre:paises[randomPais]})
+    }
+    console.log({paisesSeleccionados})
+    setElegidos(paisesSeleccionados.map((pais,i)=>{
+      return <li key={i}>{pais.nombre}</li>
+    }))
+
+    const randomCountry = paisesSeleccionados[Math.floor(Math.random()*paisesSeleccionados.length)].codigo
+    setBandera(`https://flagcdn.com/256x192/${randomCountry}.png`)
+  }
+
+
+  return (<>
+    <button onClick={handleClick}>Comenzar</button>
+    <ul>{elegidos}</ul>
+    <img src={bandera}/>
+    </>
   );
 }
 
